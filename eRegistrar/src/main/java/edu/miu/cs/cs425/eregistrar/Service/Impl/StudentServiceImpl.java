3c297dbae1c0327633af9848dca2cfb6a6fb2cd6
selam <sehabte@miu.edu>
2022-07-08 19:36:04 -0500

    lab 11-2

--- eRegistrar/src/main/java/edu/miu/cs/cs425/eregistrar/Service/Impl/StudentServiceImpl.java
+++ eRegistrar/src/main/java/edu/miu/cs/cs425/eregistrar/Service/Impl/StudentServiceImpl.java
@@ -3,24 +3,30 @@ package edu.miu.cs.cs425.eregistrar.Service.Impl;
 import edu.miu.cs.cs425.eregistrar.Repository.StudentRepository;
 import edu.miu.cs.cs425.eregistrar.Service.StudentService;
 import edu.miu.cs.cs425.eregistrar.model.Student;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 @Service
 public class StudentServiceImpl implements StudentService {
+    @Autowired
     private StudentRepository studentRepository;
-
-    public StudentServiceImpl(StudentRepository studentRepository) {
-        this.studentRepository = studentRepository;
-    }
+//
+//    public StudentServiceImpl(StudentRepository studentRepository) {
+//        this.studentRepository = studentRepository;
+//    }
 
     @Override
     public Student addStudent(Student student) {
         return studentRepository.save(student);
     }
 
+//    @Override
+//    public Iterable<Student> findAll() {
+//        return studentRepository.findAll();
+//    }
     @Override
-    public Iterable<Student> findAll() {
+    public List<Student> findAll() {
         return studentRepository.findAll();
     }
 
