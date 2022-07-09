64c2412d8bf622717f51e3dc656faeaaf4e3c771
selam <sehabte@miu.edu>
2022-07-09 08:14:32 -0500

    eregistrarRestWeb

---
+++ lab 11Rest/eRegistrarWebApp/src/main/java/edu/miu/cs/cs425/eregistrar/Service/Impl/StudentServiceImpl.java
@@ -0,0 +1,44 @@
+package edu.miu.cs.cs425.eregistrar.Service.Impl;
+
+import edu.miu.cs.cs425.eregistrar.Repository.StudentRepository;
+import edu.miu.cs.cs425.eregistrar.Service.StudentService;
+import edu.miu.cs.cs425.eregistrar.model.Student;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+public class StudentServiceImpl implements StudentService {
+   @Autowired
+    private StudentRepository studentRepository;
+
+//    public StudentServiceImpl(StudentRepository studentRepository) {
+//        this.studentRepository = studentRepository;
+//    }
+
+    @Override
+    public Student addStudent(Student student) {
+        return studentRepository.save(student);
+    }
+
+    @Override
+    public List<Student> findAll() {
+        return studentRepository.findAll();
+    }
+
+    @Override
+    public Student getStudentById(Integer studentId) {
+        return studentRepository.findById(studentId).orElse(null);
+    }
+
+    @Override
+    public void deleteStudentById(Integer studentId) {
+        studentRepository.deleteById(studentId);
+
+    }
+
+    @Override
+    public Student updateStudent(Student updated, Integer studentId) {
+        return studentRepository.save(updated);
+    }
+}
